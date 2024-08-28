74639268448af8b3e3e860b8b732ceb4c77d8e26
maudblayac <blayacmaud@laposte.net>
2024-08-28 11:20:49 +0200

    login and security are done for the moment now create new branch for the listings and cards

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,16 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+
+// style index général
 import './styles/app.css';
+
+// style restaurant
+import './styles/restaurant.css';
+
+
+// style home
+import './styles/home.css';
+
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
