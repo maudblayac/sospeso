3f623fc9ca3274745fd6439d03f083ad9aa33352
maudblayac <blayacmaud@laposte.net>
2024-11-03 19:30:48 +0100

    work on the daashboard Restaurant and the CRUD product

--- src/Controller/DashboardRestaurantController.php
+++ src/Controller/DashboardRestaurantController.php
@@ -4,15 +4,47 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\ExpressionLanguage\Expression;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/dashboard', name: 'app_dashboard_restaurant_')]
+#[IsGranted(new Expression('is_granted("ROLE_RESTAURANT") or is_granted("ROLE_ADMIN")'))]
 class DashboardRestaurantController extends AbstractController
 {
-    #[Route('/dashboard/restaurant', name: 'app_dashboard_restaurant')]
-    public function index(): Response
+    #[Route('/', name: 'home')]
+    public function home(): Response
     {
-        return $this->render('dashboard_restaurant/index.html.twig', [
-            'controller_name' => 'DashboardRestaurantController',
+        $user = $this->getUser();
+        return $this->render('dashboard_restaurant/home.html.twig', [
+            'user' => $user,
         ]);
+    }
+
+    #[Route('/profile', name: 'profile')]
+    public function profile(): Response
+    {
+        return $this->render('dashboard_restaurant/profile.html.twig');
+    }
+
+    #[Route('/products', name: 'products')]
+    public function products(EntityManagerInterface $em): Response
+    {                
+        /** @var User $user */
+        $user = $this->getUser();
+        // Récupérer les produits du restaurant du user connecté
+        $products = $em->getRepository(Product::class)->findBy(['restaurant' => $user->getRestaurant()]);
+
+        return $this->render('dashboard_restaurant/products.html.twig', [
+            'products' => $products,
+        ]);
+    }
+
+    #[Route('/restaurant', name: 'restaurant')]
+    public function restaurant(): Response
+    {
+        return $this->render('dashboard_restaurant/restaurant.html.twig');
     }
 }
